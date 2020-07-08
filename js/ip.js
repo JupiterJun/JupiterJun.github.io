be151bf143f564883ec22c4ad9d9a9efcd1b08b6
JupiterJun <hbbdmjz@163.com>
2020-07-09 06:39:53 +0800

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -1,57 +1,23 @@
-//document.write('IP地址:' + returnCitySN["cip"] + ', CID:' + returnCitySN["cid"] + ', 地区:' + returnCitySN["cname"] + ",浏览器版本:" + getBrowserInfo());
+// 记录用户IP和UA
 var pathname = window.location.pathname;
+var useragent = navigator.userAgent
 AV.init({
     appId: "VSGmCrxfAW5rz5SELuCzwOWX-gzGzoHsz",
     appKey: "pycmoYrCM6dKYG1cmwCvukct",
     serverURL: "https://vsgmcrxf.lc-cn-n1-shared.com"
   });
-// 声明 class
-const IP = AV.Object.extend('IP');
 
-// 构建对象
+const IP = AV.Object.extend('IP');
 const ip = new IP();
 
-// 为属性赋值
 ip.set('ip', returnCitySN["cip"]);
 ip.set('cid', returnCitySN["cid"]);
 ip.set('city', returnCitySN["cname"]);
-ip.set('browser', getBrowserInfo());
+ip.set('browser', useragent);
 ip.set('path',pathname);
 
-// 将对象保存到云端
 ip.save().then((ip) => {
-    // 成功保存之后，执行其他逻辑
     console.log(`objectId：${ip.id}`);
 }, (error) => {
-    // 异常处理
+
 });
-
-function getBrowserInfo() {
-    var agent = navigator.userAgent.toLowerCase();
-
-    var regStr_ie = /msie [\d.]+;/gi;
-    var regStr_ff = /firefox\/[\d.]+/gi
-    var regStr_chrome = /chrome\/[\d.]+/gi;
-    var regStr_saf = /safari\/[\d.]+/gi;
-
-    //IE
-    if (agent.indexOf("msie") > 0) {
-        return agent.match(regStr_ie);
-    }
-
-    //firefox
-    if (agent.indexOf("firefox") > 0) {
-        return agent.match(regStr_ff);
-    }
-
-    //Chrome
-    if (agent.indexOf("chrome") > 0) {
-        return agent.match(regStr_chrome);
-    }
-
-    //Safari
-    if (agent.indexOf("safari") > 0 && agent.indexOf("chrome") < 0) {
-        return agent.match(regStr_saf);
-    }
-
-}
